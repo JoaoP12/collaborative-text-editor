ebca91e082004253e10e77b6d544697e127dbf72
Joao Lopes <joao.lopes@exati.com.br>
2025-03-25 19:33:49 -0400

    Create client and server directories and added Docker

--- server/index.js
+++ server/index.js
@@ -1,13 +1,19 @@
 const WebSocket = require("ws");
 
-const server = new WebSocket.Server({
-  port: 5050,
-});
+const server = new WebSocket.Server(
+  {
+    port: process.env.PORT || 5000,
+  },
+  () => {
+    console.log("Server started on port", server.address().port);
+  }
+);
 
 let sockets = [];
 
 server.on("connection", function (socket) {
   sockets.push(socket);
+  console.log("Connected: ", socket.url);
 
   socket.on("message", function (msg) {
     console.log("Received: ", msg);
